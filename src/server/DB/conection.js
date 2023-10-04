9d7ba89b26df13c5a0a4622878cb9415e4e24678
PaulaSanchez <pausanchezt12@gmail.com>
2023-10-04 18:23:58 -0500

    changes for the deploy

--- src/server/DB/conection.js
+++ src/server/DB/conection.js
@@ -1,11 +1,18 @@
 import pkg from "pg";
+import {
+  DB_HOST,
+  DB_USER,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_PORT,
+} from "./config.conection.js";
 
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "catChat",
-  password: "postgres",
-  port: 5432,
+  user: DB_USER,
+  host: DB_HOST,
+  database: DB_NAME,
+  password: DB_PASSWORD,
+  port: DB_PORT,
 });
