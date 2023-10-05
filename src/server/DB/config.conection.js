97f976dc6fe1053ca6bb43e60092a61dd68d87df
PaulaSanchez <pausanchezt12@gmail.com>
2023-10-05 06:31:37 -0500

    fix: change for fixed

--- src/server/DB/config.conection.js
+++ src/server/DB/config.conection.js
@@ -3,3 +3,9 @@ export const DB_USER = process.env.DB_USER || "postgres";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "postgres";
 export const DB_NAME = process.env.DB_NAME || "catChat";
 export const DB_PORT = process.env.DB_PORT || 5432;
+
+console.log("DB_HOST" + DB_HOST);
+console.log("DB_USER" + DB_USER);
+console.log("DB_PASSWORD" + DB_PASSWORD);
+console.log("DB_NAME" + DB_NAME);
+console.log("DB_PORT" + DB_PORT);
